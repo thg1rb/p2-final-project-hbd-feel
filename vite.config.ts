2a6f869eea4e6441f6278811c7cc706d1aebf2cb
TeeAnusonsart <anusonsart@gmail.com>
2026-03-05 23:11:22 +0700

    test: CICD Code Analysis

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   server: {
     port: 3000
   },
-  plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
+  
+  plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}', 'tests/**/*.{test,spec}.{js,ts}'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html', 'lcov'], // 'lcov' สำคัญมากสำหรับ SonarQube
+      reportsDirectory: './coverage'
+    }
+  }
+
 });

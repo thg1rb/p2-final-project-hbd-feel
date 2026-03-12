404d4b7d9982fddd1bf4c69177593fb1281bc4b9
Bowornrat <bowornrat.t@ku.th>
2026-03-12 15:16:13 +0700

    fix: svelte config and content type

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,8 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 export default {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter()
+      adapter: adapter({
+			bodySizeLimit: 1024 * 1024 * 10
+		})
   }
 };

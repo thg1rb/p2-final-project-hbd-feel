3fd827578b936a30b4ac80b4bcbefd443ad59e1e
Bowornrat <bowornrat.t@ku.th>
2026-03-12 16:05:02 +0700

    fix: request size

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,8 +4,5 @@ import axios from 'axios';
 
 export const apiClient = axios.create({
 	baseURL: browser ? PUBLIC_BROWSER_API_BASE_URL : PUBLIC_DOCKER_API_BASE_URL,
-	timeout: 15000,
-	headers: {
-		'Content-Type': 'application/json'
-	}
+	timeout: 15000
 });
